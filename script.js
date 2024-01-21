9b6492b70a20e9854c05871fe20459f759837908
HARIOM SHUKLA <hariom09shukla@gmail.com>
2024-01-21 18:28:19 +0530

    work on to add task

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
 let ul=document.querySelector("ul")
 let btn=document.querySelector("#btn");
-let li=document.querySelectorAll(".li");
-let icn=document.querySelectorAll("#icon");
+let li=document.querySelectorAll("li");
+let icn=document.querySelector("i");
 let input=document.querySelector("#input");
-
-
- 
-
-
-
-btn.addEventListener("click",()=>{
-    let inputvalue=input.value;
-    console.log(inputvalue);
-    
+btn.addEventListener(("click"),()=>{
     if(input.value===""){
-        alert("Write Something");
-    } 
+        alert("write something");
+    }
     else{
+        let inputvalue=input.value;
+        console.log(inputvalue);
         let newLi=document.createElement("li");
-        newLi=inputvalue;
+        console.log(newLi)
+        newLi.innerHTML=inputvalue;
         ul.append(newLi);
-        
-    } 
-    input.value="";
-     
-    
+        newLi.appendChild(icn);
+    }input.value="";
 });
+ul.addEventListener(("click"),(e)=>{
+    // console.log(e.target);
+    e.target.style.textDecoration="line-through" ;
+    Color="#555" ;   
+});
+icn.addEventListener(("click"),(e)=>{
+    console.log(e.target);
+    
+    
+})
 
 
    
